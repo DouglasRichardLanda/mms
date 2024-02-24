8186d45c66cea7c0d46e80dfa71a6c4616af78f4
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-02-24 18:05:55 +0100

    Animation aus

--- app/page.js
+++ app/page.js
@@ -5,25 +5,25 @@ export default function Home() {
   return (
     <main className={`flex justify-center items-center overflow-hidden`}>
         <motion.img
-          initial={{ x: `200%`, opacity: 0 }}
-          transition={{ delay: 0.5, duration: 0.5 }}
-          animate={{ x: 0, opacity: 1}}
+          // initial={{ x: `200%`, opacity: 0 }}
+          // transition={{ delay: 0.5, duration: 0.5 }}
+          // animate={{ x: 0, opacity: 1}}
           className={`lg:w-1/3 sm:w-1/2 absolute top-[25%]`}
           src="/debut/pitbul.png"
           alt="debut release picture"
         />
         <motion.img
-          initial={{ x: `-200%`,y: `100%`, opacity: 0 }}
-          transition={{ delay: 1, duration: 0.5 }}
-          animate={{ x: 0, y: 0,opacity: 1,}}
+          // initial={{ x: `-200%`,y: `100%`, opacity: 0 }}
+          // transition={{ delay: 1, duration: 0.5 }}
+          // animate={{ x: 0, y: 0,opacity: 1,}}
           className={`absolute top-[44%] sm:right-[10%] object-cover sm:w-1/2`}
           src="/debut/outNow.png"
           alt="second picture"
         />
         <motion.img
-          initial={{ x: `200%`,y: `100%` , opacity: 0 }}
-          transition={{ delay: 1, duration: 0.5 }}
-          animate={{ x: 0,y: 0 , opacity: 1,}}
+          // initial={{ x: `200%`,y: `100%` , opacity: 0 }}
+          // transition={{ delay: 1, duration: 0.5 }}
+          // animate={{ x: 0,y: 0 , opacity: 1,}}
           className={`absolute top-[33%]  object-cover sm:w-1/2`}
           src="/debut/debut.png"
           alt="third picture"
