b39bd87ea38559394bd95f0b03480fd8b8a28033
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-03-06 19:43:25 +0100

    News

--- app/(pages)/news/page.js
+++ app/(pages)/news/page.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 function Page(props) {
   return (
-    <div className={`h-full flex flex-col items-center gap-10`}>
-      <p>7th March ‘23: Official debut – 3 bran new song out! Stream or download now!</p>
+    <div className={`h-full flex flex-col gap-4 bg-white rounded text-black p-4 tracking-wider shadow-md shadow-white text-lg`}>
+      <h1 className={`text-3xl`}>7 March 2024</h1>
+      <p>Hurrah! After long lasting hard work we - Mad Man’s Spirit celebrate our online debut! To all lovers of rock,
+        punk rock and simply music - be prepared for delicious sounding music, that we have recorded specially for
+        you!
+      </p>
+      <p>
+        Huge thanks to all, who supported us and collaborated with us in creation of tones and notes, sounds and
+        pictures!
+      </p>
+      <p>
+        We are very glad for having the opportunity to work with Grammy winners and grateful to Tom Lord-Aldge
+        for the unbelievable mix and Ted Jensen from Sterling Sound for great mastering of our sound!
+      </p>
+      <p>
+        Today, dear rock and punk rock fans, we present to you our first song from the single, with total three tracks,
+        called – “Pit Bull”!
+        The video is out! It was made with help of AI. Its imperfection gives a kind of artistic touch to the pictures
+        you will see! We had a lot of fun making it for you! So enjoy!
+      </p>
+      <p>
+        Wanna some more? Stay tuned! The next surprises are already on their way!
+      </p>
     </div>
   );
 }
