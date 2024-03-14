2d05623a4ce6f453eb123e87080bc9567fb6624e
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-03-15 00:15:45 +0100

    production build

--- app/_actions/mail.js
+++ app/_actions/mail.js
@@ -3,7 +3,7 @@
 export default async function mailer (data) {
 
   const {email, message} = data
-  const res = await fetch('http://localhost:3000/api/mailer', {
+  const res = await fetch('https://www.madmansspirit.com/api/mailer', {
     method: 'POST',
     body: JSON.stringify({email, message})
   })
