d33278e680b398ef880a2d17303e7700997f4cbc
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-04-01 22:43:04 +0200

    bug fixed

--- app/(pages)/news/wrapper.js
+++ app/(pages)/news/wrapper.js
@@ -27,8 +27,11 @@ export default function Wrapper (props) {
   const isSubmitted = async (data) => {
     data.preventDefault()
     const formData = new FormData(data.target)
+    if (formData.get('text') === '') {
+      setUnvisible()
+      return
+    }
     await action_news_add(formData)
-    setUnvisible()
   }
 
   return (
@@ -40,8 +43,8 @@ export default function Wrapper (props) {
         <input type="text" name={'title'} className={`p-2 rounded text-black w-full`} placeholder={'Title'}/>
         <textarea name={'text'} id="" cols="30" rows="15" className={`text-black p-2 rounded`} placeholder={` Content. \n Use ENTER as a new paragraph.`}></textarea>
         <div className={`flex justify-around items-center`}>
-          <button className={`hover:bg-green-600 duration-200 py-2 w-1/3 rounded`} type={"submit"}>Submit</button>
-          <button className={`hover:bg-red-600 duration-200 py-2 w-1/3 rounded`} onClick={setUnvisible}>Close</button>
+          <button className={`hover:bg-green-600 duration-200 py-2 w-1/3 rounded`}>Submit</button>
+          <button className={`hover:bg-red-600 duration-200 py-2 w-1/3 rounded`} type={"submit"} onClick={setUnvisible}>Close</button>
         </div>
       </form>
     </div>
