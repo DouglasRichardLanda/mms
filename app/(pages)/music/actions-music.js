c86b8b6dec92447c405db70e3262d0a6558e24bf
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-04-06 20:35:32 +0200

    fs

--- app/(pages)/music/actions-music.js
+++ app/(pages)/music/actions-music.js
@@ -2,15 +2,24 @@
 import prisma from "@/prisma/prismaconnect";
 import {join} from "path"
 import {writeFile} from "fs/promises"
+
 export async function action_music_add(data) {
   const file = data.get('file')
   const name = data.get('name')
   const bytes = await file.arrayBuffer()
   const buffer = Buffer.from(bytes)
 
-  const path = join(process.cwd(), 'public', 'albums', file.name);
+  // // Point to 'uploads' one directory above
+  // let dir = path.join(process.cwd(), '..', 'uploads');
+  // // Create directory if it doesn't exist.
+  // if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  // // Set path of the image
+  // file.path = path.join(dir, file.name);
+
+
+  // const path = join(process.cwd(), 'public', 'albums', file.name);
+  const path = join('/var/task', 'public', 'albums', file.name);
   await writeFile(path, buffer);
-  console.log("PATH:::", path)
 
   await prisma.Album.create({
     data: {
